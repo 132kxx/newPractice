5571e96d559b7bddae4d8e81c26e918c39928c9d
kxx <132kxx@gmail.com>
2023-03-06 11:09:48 +0900

    Initial Commit

---
+++ newPractice/newPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  newPracticeApp.swift
+//  newPractice
+//
+//  Created by kxx: on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct newPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
